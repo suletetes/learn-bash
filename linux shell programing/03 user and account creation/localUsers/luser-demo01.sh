ecde53c34810a88d9e8a7d3aaa1f6548dfa905bf
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-11-23 16:13:25 +0000

    updated

--- linux shell programing/03 user and account creation/localUsers/luser-demo01.sh
+++ linux shell programing/03 user and account creation/localUsers/luser-demo01.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# hello from vim
+# hello from the main os
 echo "hello"
 
 # assign variable
@@ -7,3 +7,22 @@ WORD='script'
 
 # Display that value using variable
 echo "$WORD"
+
+echo "This is a shell $WORD"
+
+echo "This is a shell {$WORD}"
+
+# Append text to variable
+echo "${WORD}ing is fun!"
+
+# create a new varaible
+ending='ed'
+
+# combine the two variables
+echo "This is ${WORD}${ending}."
+
+# change the value in the ending variable (reassignment)
+ending='ing'
+echo "This is ${WORD}${ending} is fun!"
+
+

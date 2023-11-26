fe9ee25207efab4732adba3b2fa731ce51339c0d
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-11-26 15:56:59 +0000

    adedd

--- linux shell programing/03 user and account creation/localUsers/add-local-user.sh
+++ linux shell programing/03 user and account creation/localUsers/add-local-user.sh
@@ -25,3 +25,24 @@ fi
 
 # set password
 echo ${password} } passwd --stdin ${user_name}
+
+if [[ "${?}" -ne 0 ]]
+then
+    echo 'The password for this account could not be set.'
+    exit 1
+fi
+
+
+# force password change on first login
+passwd -e ${user_name}
+
+# display the username, password, and the host where the user was created
+echo
+echo 'username:'
+echo "${user_name}"
+echo
+echo 'password:'
+echo "${password}"
+echo 'host'
+echo "${HOSTNAME}"
+exit 0

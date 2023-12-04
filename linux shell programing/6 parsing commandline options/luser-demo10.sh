7c2e5b5ac33123aa36c7039f77ae1ea73a978142
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-12-04 20:37:51 +0000

    update

--- linux shell programing/6 parsing commandline options/luser-demo10.sh
+++ linux shell programing/6 parsing commandline options/luser-demo10.sh
@@ -11,6 +11,23 @@ log(){
     then
         echo "${message}"
     fi
+    logger -t luser-demo10.sh "${message}"
+}
+
+backup_file(){
+    local file="${1}"
+    # make sure file exists
+    if [[ -f "${file}" ]]
+    then
+        local BACKUP_FILE="/var/tmp/$(basename ${file}).$(date +%F-%N)"
+        log "Backing up ${file} to ${BACKUP_FILE}."
+        
+        # the exit status of the function will be the exit status of the cp command
+        cp -p ${file} ${BACKUP_FILE}
+    else
+        # the file does not exist, so retturn a non zero exit status
+        return 1
+    fi
 }
 
 # log 'true' 'Hello'
@@ -20,3 +37,5 @@ log(){
 verbosity='true'
 log "${verbosity}" "hello!"
 log "${verbosity}" "this is fun!"
+
+backup_file

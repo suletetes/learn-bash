9ee092980287a98b621ba6712528ec7b91e766b1
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-12-04 16:44:12 +0000

    update

--- linux shell programing/5 linux programming conventions project 3/add-newer-local-user.sh
+++ linux shell programing/5 linux programming conventions project 3/add-newer-local-user.sh
@@ -9,14 +9,14 @@
 # Make sure the script is being executed with superuser privileges.
 if [[ "${UID}" -ne 0 ]]
 then
-   echo 'Please run with sudo or as root.'
+   echo 'Please run with sudo or as root.' >&2
    exit 1
 fi
 
 # If the user doesn't supply at least one argument, then give them help.
 if [[ "${#}" -lt 1 ]]
 then
-  echo "Usage: ${0} USER_NAME [COMMENT]..."
+  echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
   echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT.'
   exit 1
 fi
@@ -32,28 +32,28 @@ COMMENT="${@}"
 PASSWORD=$(date +%s%N | sha256sum | head -c48)
 
 # Create the user with the password.
-useradd -c "${COMMENT}" -m ${USER_NAME}
+useradd -c "${COMMENT}" -m ${USER_NAME} &> /dev/null
 
 # Check to see if the useradd command succeeded.
 # We don't want to tell the user that an account was created when it hasn't been.
 if [[ "${?}" -ne 0 ]]
 then
-  echo 'The account could not be created.'
+  echo 'The account could not be created.' >&2
   exit 1
 fi
 
 # Set the password.
-echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+echo ${PASSWORD} | passwd --stdin ${USER_NAME} &> /dev/null
 
 # Check to see if the passwd command succeeded.
 if [[ "${?}" -ne 0 ]]
 then
-  echo 'The password for the account could not be set.'
+  echo 'The password for the account could not be set.' >&2
   exit 1
 fi
 
 # Force password change on first login.
-passwd -e ${USER_NAME}
+passwd -e ${USER_NAME} &> /dev/null
 
 # Display the username, password, and the host where the user was created.
 echo

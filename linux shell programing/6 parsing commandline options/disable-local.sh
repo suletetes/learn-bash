fb2669bafa3b49f9850c286842af3d0f50599778
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-12-15 00:55:19 +0000

    added

--- linux shell programing/6 parsing commandline options/disable-local.sh
+++ linux shell programing/6 parsing commandline options/disable-local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #  the script disables, deletes, and/or archives users on the local systme
+ARCHIVE_DIR='/archive'
 usage(){
     # Display the usage and exit.
     echo "Usage: ${0} [-dra] user [usern]" >&2
@@ -50,3 +51,37 @@ do
         echo "Refusing to remove the ${USERNAME} account with UID ${USERID}." >&2
         exit 1
     fi
+
+    # create an archive if requested to do so.
+    if [[ "${ARCHIVE}" = 'true' ]]
+    then
+    # make sure the archive_dir directory exists.
+    if [[ ! -d "${ARCHIVE_DIR}" ]]
+    then
+        echo "creating ${ARCHIVE_DIR} directory."
+        mkdir -p ${ARCHIVE_DIR}
+        if [[ "${?}" -ne 0 ]]
+        then
+            echo "The archive directory ${ARCHIVE_DIR} could not be created." >&2
+            exit 1
+        fi
+    fi
+    # archive the user's home directory and move it into the ARCHIVE_DIR
+    HOME_DIR="/home/${USERNAME}"
+    ARCHIVE_FILE="${ARCHIVE_DIR}/${USERNAME}.tgz"
+    if [[ -d "${HOME_DIR}" ]]
+    then
+        echo "Archiving ${HOME_DIR} to ${ARCHIVE_FILE}"
+        tar -zcf ${ARCHIVE_FILE} ${HOME_DIR} &> /dev/null
+        if [[ "${?}" -eq 0 ]]
+        then
+            echo "could not create ${ARCHIVE_FILE}.">&2
+            exit 1
+        fi
+    else
+        echo "${HOME_DIR} does not exist or is not a directory." >&2
+    fi
+fi 
+
+
+

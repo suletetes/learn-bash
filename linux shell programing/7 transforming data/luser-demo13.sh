5f3ec0d5a3d96b54f79b2a1caff29aa805068151
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-12-15 16:23:02 +0000

    update

---
+++ linux shell programing/7 transforming data/luser-demo13.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#  this script shows the open network ports on a system.
+#  use -4 as an arguement to limit tcpv4 ports
+
+netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}'

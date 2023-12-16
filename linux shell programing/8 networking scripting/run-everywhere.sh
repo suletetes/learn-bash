3eaa3c966bc30f2c0d5573e6c1072913f9f06e2d
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-12-16 16:00:05 +0000

    update

--- linux shell programing/8 networking scripting/run-everywhere.sh
+++ linux shell programing/8 networking scripting/run-everywhere.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+#  a list of servers, one per line.
+SERVER_LIST='servers'
+
+# options for ssh command
+
+SSH_OPTIONS='-o ConnectTimeout=2'
+
+
+usage(){
+    # display the usage and exit
+    echo "usage: ${0} [-nsv] [-f FILE] command" >&2
+    echo "executes command as a single command on every server" >&2
+}
+# make sure the script is not begin executed with suoeruse privileges
+if [[ "${UID}" -eq 0 ]]
+then
+    echo "Do not execute this script as root. use the -s option instead." >&2
+    usage
+fi

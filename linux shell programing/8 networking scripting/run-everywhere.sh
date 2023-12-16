691403c4f1a76194c7afe8bac5dc43db53e3740e
Suleiman Abdulkadir <85356587+suletetes@users.noreply.github.com>
2023-12-16 16:03:28 +0000

    update

--- linux shell programing/8 networking scripting/run-everywhere.sh
+++ linux shell programing/8 networking scripting/run-everywhere.sh
@@ -12,6 +12,11 @@ usage(){
     # display the usage and exit
     echo "usage: ${0} [-nsv] [-f FILE] command" >&2
     echo "executes command as a single command on every server" >&2
+    echo "  -f file use file for the list of severs. Default ${SERVER_LIST}" >&2
+    echo " -n  dry run mode. display the command that would have been executed and exit" >&2
+    echo " -s   execute the command using sudo on the remote server" >&2
+    echo " -v verbose mode. Displays the server name before executing command" >&2
+    exit 1
 }
 # make sure the script is not begin executed with suoeruse privileges
 if [[ "${UID}" -eq 0 ]]
